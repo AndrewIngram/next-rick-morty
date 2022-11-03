92a934be713c23d35b4748d8cdc786924c47a87c
Andrew Ingram <andy@andrewingram.net>
2022-11-03 01:00:56 +0000

    tweaks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rick and Morty API frontend w/Next.js
 
 Quick and cheerful demo of React Server Components, uses Dataloader for batching characters on each episode card
+
+https://next-rick-morty.vercel.app/
--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -9,8 +9,13 @@ interface Props {
 export default function EpisodeCard({ episode }: Props) {
   return (
     <article className="flex flex-col gap-3 bg-white p-4 rounded-md shadow-sm h-full">
-      <hgroup>
-        <h2 className="font-semibold">{episode.name}</h2>
+      <hgroup className="flex flex-col gap-1">
+        <div className="flex gap-2 items-baseline">
+          <span className="bg-lime-500 text-white font-bold text-sm rounded-full w-5 h-5 flex flex-none items-center justify-center">
+            {episode.episode}
+          </span>
+          <h2 className="font-semibold">{episode.name}</h2>
+        </div>
         <h3 className="text-sm">{episode.airDate}</h3>
       </hgroup>
       <ul className="flex gap-1">
